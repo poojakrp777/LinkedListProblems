0f5dda21420d255dae885a5567695a6c901d405a
poojakrp777 <poojakrp777@gmail.com>
2023-02-14 13:17:53 +0530

    [POOJA] creating a simple linked list

---
+++ LinkedListProblem/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListProblem
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To the Data structure program using Linked List");
+
+            LinkListOperation listOperation = new LinkListOperation();
+            listOperation.insert(56);
+            listOperation.insert(30);
+            listOperation.insert(70);
+            listOperation.display();
+            Console.ReadLine();
+        }
+    }
+}
